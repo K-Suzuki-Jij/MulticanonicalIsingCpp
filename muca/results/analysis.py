1cc582b74c90c227f9670517705549669f3dcb47
KSuzuki <eixecosxpisinx@gmail.com>
2024-06-18 01:11:40 +0900

    Faster expectation calc.

--- muca/results/analysis.py
+++ muca/results/analysis.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import mpmath
 import numpy as np
 
@@ -6,6 +7,36 @@ class Analyzer:
 
     @classmethod
     def calculate_expectation(
+        cls,
+        energies: np.ndarray,
+        entropies: np.ndarray,
+        temperature_list: np.ndarray,
+        values: np.ndarray,
+    ) -> np.ndarray:
+        """Calculate the expectation value of a given observable.
+
+        Args:
+            energies (np.ndarray): The energies of the system.
+            entropies (np.ndarray): The entropies of the system.
+            temperature_list (np.ndarray): The list of temperatures to calculate the expectation value for.
+            values (np.ndarray): The values of the observable.
+
+        Returns:
+            np.ndarray: The expectation value of the observable for given temperatures.
+        """
+        min_value = np.min(values) - 0.1
+        scaled_values = values - min_value
+        result = np.zeros(len(temperature_list))
+        for i, temperature in enumerate(temperature_list):
+            exponents = -energies / temperature + entropies
+            exponents -= np.max(exponents)
+            a = reduce(np.logaddexp, exponents + np.log(scaled_values))
+            b = reduce(np.logaddexp, exponents)
+            result[i] = np.exp(a - b) + min_value
+        return result
+
+    @classmethod
+    def calculate_expectation_mpmath(
         cls,
         energies: np.ndarray,
         entropies: np.ndarray,
@@ -36,36 +67,6 @@ class Analyzer:
         return result
 
     @classmethod
-    def calculate_energy_expectation(
-        cls,
-        energies: np.ndarray,
-        entropies: np.ndarray,
-        temperature_list: np.ndarray,
-        power: int = 1,
-    ) -> np.ndarray:
-        """Calculate the internal energy of the system.
-
-        Args:
-            data (Union[WangLandauResults, MulticanonicalResults]): The data obtained from the simulation.
-            temperature_list (np.ndarray): The list of temperatures to calculate the internal energy for.
-            power (int, optional): The power to raise the energy to. Defaults to 1.
-
-        Returns:
-            np.ndarray: The internal energy of the system for given temperatures.
-        """
-        internal_energies = np.zeros(len(temperature_list))
-        for i, temperature in enumerate(temperature_list):
-            exponents = -energies / temperature + entropies
-            exponents -= np.max(exponents)
-            partition_function = mpmath.mpf(0)
-            expectation = mpmath.mpf(0)
-            for e, s in zip(energies, exponents):
-                partition_function += mpmath.exp(s)
-                expectation += (e**power) * mpmath.exp(s)
-            internal_energies[i] = float(expectation / partition_function)
-        return internal_energies
-
-    @classmethod
     def calculate_heat_capacity(
         cls,
         energies: np.ndarray,
@@ -79,8 +80,8 @@ class Analyzer:
         Returns:
             np.ndarray: The heat capacity of the system for given temperatures.
         """
-        e = cls.calculate_energy_expectation(energies, entropies, temperature_list)
-        ee = cls.calculate_energy_expectation(
-            energies, entropies, temperature_list, power=2
+        e = cls.calculate_expectation(energies, entropies, temperature_list, energies)
+        ee = cls.calculate_expectation(
+            energies, entropies, temperature_list, energies**2
         )
         return (ee - e**2) / (temperature_list**2)
